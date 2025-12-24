05895865bcfde643bccf8fdd154f78f67414e3ea
AmirJ <64044992+amirjimdaniels@users.noreply.github.com>
2025-12-24 10:32:17 -0600

    Creating register.jsx
    Making other file changes to app.tsx. Adding accessibility features

--- Aurora socials/src/App.tsx
+++ Aurora socials/src/App.tsx
@@ -1,13 +1,38 @@
+import React from 'react';
 import Navbar from "./assets/components/Navbar.jsx";
+import Register from "./assets/Register.jsx";
+
+
+function Home() {
+  return (
+    <div style={{ padding: '6rem 1rem 1rem 1rem', textAlign: 'center' }}>
+      <h1>Welcome to Aurora Socials</h1>
+      <p>This is the home page. Use the navigation bar to explore.</p>
+    </div>
+  );
+}
+  // TODO: Add React Router for navigation between Home, Register, etc.
+  // function Home() {
+  //   return (
+  //     <div style={{ padding: '6rem 1rem 1rem 1rem', textAlign: 'center' }}>
+  //       <h1>Welcome to Aurora Socials</h1>
+  //       <p>This is the home page. Use the navigation bar to explore.</p>
+  //     </div>
+  //   );
+  // }
+
+
 
 const App = () => {
   return (
-    <div className = 'container'>
+    <>
       <Navbar />
-      {/* Other components and content */}
-    </div>
+      <div className='container' style={{ paddingTop: '60px' }}>
+        <Home />
+        <Register />
+      </div>
+    </>
   );
-
 }
 
-  export default App;
+export default App;
